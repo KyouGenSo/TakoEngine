6bfa5e90bc74f8e4f1fe8e734f5afb7a8cb94fb8
KyouGenSo <k023g0083@g.neec.ac.jp>
2025-02-26 14:49:02 +0900

    Add TransFormNormal
    Mat4x4Func.cppにTransFormNormal関数関数を追加しました。
    TransFormNormal関数はMatrix4x4とVector3を引数に取り、法線ベクトルを変換してVector3を返します。

--- project/engine/func/mathFunc/Mat4x4Func.h
+++ project/engine/func/mathFunc/Mat4x4Func.h
@@ -40,6 +40,8 @@ namespace Mat4x4 {
 
 	Vector3 TransForm(const Matrix4x4& m, const Vector3& v);
 
+  Vector3 TransFormNormal(const Matrix4x4& m, const Vector3& v);
+
 	Matrix4x4 MakePerspective(float fovY, float aspectRatio, float nearClip, float farClip);
 
 	Matrix4x4 MakeOrtho(float left, float top, float right, float bottom, float nearClip, float farClip);
