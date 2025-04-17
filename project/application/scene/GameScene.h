d5f67a43b7339cae7f90f79874f95e6549a30883
KyouGenSo <k023g0083@g.neec.ac.jp>
2025-04-17 10:17:01 +0900

    CG4_K01_02完成

--- project/application/scene/GameScene.h
+++ project/application/scene/GameScene.h
@@ -34,59 +34,6 @@ public: // メンバ関数
 
 private: // メンバ変数
 
-  // スポットライトデータ
-  struct SpotLight
-  {
-    Vector4 color;
-    Vector3 position;
-    float intensity;
-    Vector3 direction;
-    float distance;
-    float decay;
-    float cosAngle;
-    bool enable;
-  };
-
-  // 点光源データ
-  struct PointLight
-  {
-    Vector4 color;
-    Vector3 position;
-    float intensity;
-    float radius;
-    float decay;
-    bool enable;
-  };
-
-  Object3d* object3d_ = nullptr;
-  Object3d* object3d2_ = nullptr;
-
   bool isDebug_ = false;
 
-  // モデルの設定
-  Vector3 modelScale_ = { .x = 1.0f, .y = 1.0f, .z = 1.0f };
-  Vector3 modelPos_ = { .x = 0.0f, .y = 0.0f, .z = 0.0f };
-  Vector3 modelRotate_ = { .x = 0.0f, .y = 0.0f, .z = 0.0f };
-
-  Vector3 modelScale2_ = { .x = 1.0f, .y = 1.0f, .z = 1.0f };
-  Vector3 modelPos2_ = { .x = 0.0f, .y = 0.0f, .z = 0.0f };
-  Vector3 modelRotate2_ = { .x = 0.0f, .y = 0.0f, .z = 0.0f };
-
-  // 平行光源の設定
-  float shininess_ = 100.0f;
-  bool isLighting_ = true;
-  bool isHighlight_ = true;
-  Vector4 lightColor_ = { .x = 1.0f, .y = 1.0f, .z = 1.0f, .w = 1.0f };
-  Vector3 lightDirection_ = { .x = 0.0f, .y = -1.0f, .z = 0.0f };
-  float lightIntensity_ = 1.0f;
-
-    // エミッター管理
-  std::unique_ptr<EmitterManager> emitterManager_;
-
-  // エミッター設定
-  SphereEmitterParams spEmitterSett_ = {};
-  BoxEmitterParams boxEmitterSett_ = {};
-  TriangleEmitterParams triEmitterSett_ = {};
-  Vector3 groupPosition_ = {};
-  bool isActive_ = true;
 };
