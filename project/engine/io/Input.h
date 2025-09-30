e9e657db448c884b841b649fe9780bfe2bd77778
KyouGenSo <k023g0083@g.neec.ac.jp>
2025-09-30 12:14:53 +0900

    DIRECTINPUT_VERSION再定義コメントアウト

--- project/engine/io/Input.h
+++ project/engine/io/Input.h
@@ -5,7 +5,7 @@
 #include "Xinput.h"
 #pragma comment(lib, "XInput.lib")
 
-#define DIRECTINPUT_VERSION 0x0800 // DirectInputのバージョン指定
+//#define DIRECTINPUT_VERSION 0x0800 // DirectInputのバージョン指定
 #include <dinput.h>
 #pragma comment(lib, "dinput8.lib")
 #pragma comment(lib, "dxguid.lib")
